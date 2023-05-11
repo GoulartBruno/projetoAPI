3553403acd5ddeec5ff2f26ab0a2dfddc3731efd
GoulartBruno <brunogoulartdev@gmail.com>
2023-05-11 11:33:55 +0200

    changes header

--- projeto-api/src/components/header/Header.js
+++ projeto-api/src/components/header/Header.js
@@ -1,17 +1,17 @@
 import { Flex, Button, Image } from "@chakra-ui/react";
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import image from "../../assets/logo.png";
+import image from "../../assets/image.png";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
-import { goToPokedex, goHome } from "../../routes/cordinator";
+import { goHome, goToPokedex } from "../../routes/cordinator";
 
 function Header() {
   const navigate = useNavigate();
   const location = useLocation();
 
   const renderHeader = () => {
-    switch (location.pathname) {
-      case "/":
+    switch (true) {
+      case location.pathname === "/":
         return (
           <Flex justifyContent={"space-around"} alignItems={"center"}>
             <Image src={image} alt="image logo pokemon" />
@@ -19,17 +19,17 @@ function Header() {
               w={"16rem"}
               h={"4rem"}
               br={"2rem"}
-              bg={"#33a4f5"}
+              bg={"#33A4F5"}
               color={"white"}
               onClick={() => {
-                goToPokedex(navigate, "pokedex");
+                goToPokedex(navigate);
               }}>
               Pokédex
             </Button>
           </Flex>
         );
-        break;
-      case "/details/:details":
+
+      case location.pathname.indexOf("details") > -1:
         return (
           <Flex justifyContent={"space-around"} alignItems={"center"}>
             <Button
@@ -40,24 +40,16 @@ function Header() {
               fontSize={"1.5rem"}
               as={"u"}
               onClick={() => {
-                goHome(navigate, "/");
+                goHome(navigate);
               }}>
               <ChevronLeftIcon />
-              Todos os Pokemons
+              All Pokemons
             </Button>
             <Image src={image} alt="image logo pokemon" />
-            <Button
-              w={"16rem"}
-              h={"4rem"}
-              br={"2rem"}
-              bg={"#FF6262"}
-              color={"white"}>
-              Excluir da Pokédex
-            </Button>
           </Flex>
         );
-        break;
-      case "/pokedex/:pokedex":
+
+      case location.pathname.indexOf("pokedex") > -1:
         return (
           <Flex justifyContent={"space-around"} alignItems={"center"}>
             <Button
@@ -68,50 +60,19 @@ function Header() {
               fontSize={"1.5rem"}
               as={"u"}
               onClick={() => {
-                goHome(navigate, "/");
+                goHome(navigate);
               }}>
               <ChevronLeftIcon />
-              Todos os Pokemons
+              All Pokemons
             </Button>
             <Image src={image} alt="image logo pokemon" />
           </Flex>
         );
       default:
-        return console.log("erro no header");
+        return console.log("error on header");
     }
   };
   return <>{renderHeader()}</>;
-
-  //   <Button
-  //     w={"16rem"}
-  //     h={"4rem"}
-  //     br={"2rem"}
-  //     bg={"none"}
-  //     fontSize={"1.5rem"}
-  //     as={"u"}
-  //     onClick={() => {
-  //       goHome(navigate, "/");
-  //     }}>
-  //     <ChevronLeftIcon />
-  //     Todos os Pokemons
-  //   </Button>
-  //   <Image src={image} alt="image logo pokemon" />
-  //   <Button
-  //     w={"16rem"}
-  //     h={"4rem"}
-  //     br={"2rem"}
-  //     bg={"#33a4f5"}
-  //     color={"white"}
-  //     onClick={() => {
-  //       goToPokedex(navigate, "pokedex");
-  //     }}>
-  //     Pokédex
-  //   </Button>
-  //   <Button w={"16rem"} h={"4rem"} br={"2rem"} bg={"#FF6262"} color={"white"}>
-  //     Excluir da Pokédex
-  //   </Button>
-  // </Flex>
-  //);
 }
 
 export default Header;
